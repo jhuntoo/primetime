588b800015bc841feeb6b66e4fe2ecaf70a4c6e0
Jonathon Lee <jonnolee84@gmail.com>
2015-10-04 16:42:13 +0100

    Prime Numbers - Created expectations for generating a list of prime numbers

---
+++ src/PrimeTime.Tests.Unit/PrimeNumberTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace PrimeTime.Tests.Unit
+{
+    [TestFixture]
+    public class PrimeNumberTests
+    {
+        [Test]
+        public void when_zero_primes_are_requested_return_empty_list()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void when_1_prime_is_requested_return_the_number_one()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void when_5_primes_are_requested_return_1_2_3_5_7()
+        {
+            Assert.Fail();
+        }
+    }
+}
