63bbf50975af6df99830979465d9c984bfd4109b
Jonathon Lee <jonnolee84@gmail.com>
2015-10-04 17:04:18 +0100

    Prime Numbers - Implemented the single prime number case

--- src/PrimeTime.Tests.Unit/PrimeNumberTests.cs
+++ src/PrimeTime.Tests.Unit/PrimeNumberTests.cs
@@ -13,13 +13,13 @@ namespace PrimeTime.Tests.Unit
         }
 
         [Test]
-        public void when_1_prime_is_requested_return_the_number_one()
+        public void when_1_prime_is_requested_return_the_number_two()
         {
-            Assert.Fail();
+            PrimeList.With(1).Should().BeEquivalentTo(new [] {2});
         }
 
         [Test]
-        public void when_5_primes_are_requested_return_1_2_3_5_7()
+        public void when_5_primes_are_requested_return_2_3_5_7_11()
         {
             Assert.Fail();
         }
--- src/PrimeTime/PrimeList.cs
+++ src/PrimeTime/PrimeList.cs
@@ -7,10 +7,48 @@ namespace PrimeTime
     {
         private static readonly List<int> EmptyList = new List<int>(); 
 
-        public static List<int> With(int numberOfElement)
+        public static List<int> With(int numberOfElements)
         {
-            if (numberOfElement == 0) return EmptyList;
-            throw new NotImplementedException();
+            if (numberOfElements == 0) return EmptyList;
+
+            var primes = new List<int>();
+            primes.Add(2);
+            int nextPrime = 3;
+            while (primes.Count < numberOfElements)
+            {
+                int sqrt = (int)Math.Sqrt(nextPrime);
+                bool isPrime = true;
+                for (int i = 0; (int)primes[i] <= sqrt; i++)
+                {
+                    if (nextPrime % primes[i] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    primes.Add(nextPrime);
+                }
+                nextPrime += 2;
+            }
+            return primes;
+
+        }
+
+        static bool IsPrimeNumber(int num)
+        {
+            bool bPrime = true;
+            int factor = num / 2;
+
+            int i = 0;
+
+            for (i = 2; i <= factor; i++)
+            {
+                if ((num % i) == 0)
+                    bPrime = false;
+            }
+            return bPrime;
         }
     }
 }
