91587a2d46aa8e2487a24ed010d31a3e9a78483b
Jonathon Lee <jonnolee84@gmail.com>
2015-10-04 17:17:43 +0100

    Prime Table - Created set of expectations for the prime table

---
+++ src/PrimeTime.Tests.Unit/PrimeTableTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace PrimeTime.Tests.Unit
+{
+    [TestFixture]
+    public class PrimeTableTests
+    {
+        [Test]
+        public void when_requested_size_is_zero_then_return_an_empty_table()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void when_requested_size_is_1_then_return_a_single_cell_table_with_the_value_1()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void when_requested_size_is_3_then_return_a_3x3_table_with_the_products_calculated()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void when_requested_size_is_5_then_return_a_5x5_table_with_the_products_calculated()
+        {
+            Assert.Fail();
+        }
+    }
+}
